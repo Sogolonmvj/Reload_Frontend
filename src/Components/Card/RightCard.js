0a3d456e2cceb9efcab2b85086447362803b2e3c
Sogolon <sogolonmvj@yahoo.com>
2022-03-30 00:47:45 -0300

    Modified left cards!

--- src/Components/Card/RightCard.js
+++ src/Components/Card/RightCard.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-import { CardStyle, Division, Image, Name } from '../../Styles/card/rightCard';
+import { Card, CardStyle, Division, Image, Name } from '../../Styles/card/rightCard';
 
 const RightCard = (props) => {
 
     if (!props) return null;
 
   return (
-    <>
+    <Card>
         <CardStyle className="card">
             <Image src={props.image} alt={props.alt} />
-            <Division>
-                <Name>{props.name}</Name>
-            </Division>
         </CardStyle>
-    </>
+        <Division>
+            <Name>{props.name}</Name>
+        </Division>
+    </Card>
   )
 }
 
--- src/Styles/card/rightCard.js
+++ src/Styles/card/rightCard.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    text-align: center;
+
+    margin: 4px 20px;
+`
+
 export const CardStyle = styled.div`
     display: flex;
     flex-direction: column;
@@ -13,8 +23,8 @@ export const CardStyle = styled.div`
     border-radius: 200px;
     box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.3), 0 0.0625rem 0.125rem rgba(0, 0, 0, 0.2);
 
-    margin-right: 20px;
-    margin-bottom: 20px;
+    height: 80px;
+    width: 80px;
 
     padding: 0;
 
@@ -29,14 +39,18 @@ export const CardStyle = styled.div`
             filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(275deg) brightness(103%) contrast(104%);
         }
     }
+
 `
 
 export const Division = styled.div`
     display: flex;
     flex-direction: row;
 
+    align-items: center;
+    justify-content: center;
+
     margin: 0;
-    padding: 5px;
+    padding: 0;
 `
 
 export const Image = styled.img`
@@ -44,12 +58,11 @@ export const Image = styled.img`
     height: 25px;
 
     padding: 0;
-    margin-top: 10px;
 `
 
 export const Name = styled.p`
     font-weight: bold;
-    font-size: 18px;
-    
-    padding: 2px 4px;
+    font-size: 14px;
+
+    width: 70px;
 `
